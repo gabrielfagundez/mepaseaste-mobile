8b483120eaa9a651f74afb8c16651eaa185ce762
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2013-11-29 21:02:36 -0200

    Agrego compatibilidad con splash screens

--- www/js/index.js
+++ www/js/index.js
@@ -21,6 +21,7 @@ var app = {
     initialize: function() {
         this.bindEvents();
     },
+
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
@@ -28,6 +29,7 @@ var app = {
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
+
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
@@ -35,6 +37,7 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
     },
+
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
