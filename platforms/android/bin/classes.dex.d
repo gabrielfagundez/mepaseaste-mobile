08c392c71f3e0914778fad27753025bf42853725
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2013-11-30 21:50:17 -0200

    Agrego swipe entre paginas

--- platforms/android/bin/classes.dex.d
+++ platforms/android/bin/classes.dex.d
@@ -6,4 +6,5 @@
 /Users/gabrielfagundez/ProyectosPersonales/mepaseaste-mobile/platforms/android/bin/classes/com/mepaseaste/uy/mobile/R$string.class \
 /Users/gabrielfagundez/ProyectosPersonales/mepaseaste-mobile/platforms/android/bin/classes/com/mepaseaste/uy/mobile/R$xml.class \
 /Users/gabrielfagundez/ProyectosPersonales/mepaseaste-mobile/platforms/android/bin/classes/com/mepaseaste/uy/mobile/R.class \
+/Users/gabrielfagundez/ProyectosPersonales/mepaseaste-mobile/platforms/android/bin/classes/org/apache/cordova/splashscreen/SplashScreen.class \
 /Users/gabrielfagundez/ProyectosPersonales/mepaseaste-mobile/platforms/android/bin/dexedLibs/cordova-3.2.0-05c0a6b0d54c5a11b0ded7d62061c241.jar \
